e0f6234753ccedfe44ee1763e4792cc678e57b07
SHIVAM SHANKHDHAR <s.shankhdhar1981@gmail.com>
2024-07-17 21:57:11 +0530

    login added to clent

--- client/app/components/Global/ShareBtn.tsx
+++ client/app/components/Global/ShareBtn.tsx
@@ -24,7 +24,7 @@ const ShareBtn = () => {
 
   return (
     <Button
-      className={`min-w-[fit-content] focus:ring-4 focus:outline-none focus:ring-purple-300 flex-1 rounded-sm px-2 flex gap-2 py-0 justify-center cursor-pointer items-center border  ${
+      className={`min-w-[fit-content] focus:ring-2 focus:outline-none focus:ring-purple-300 flex-1 rounded-sm px-2 flex gap-2 py-0 justify-center cursor-pointer items-center border  ${
         linkCopied
           ? "text-purple-800 bg-purple-100 border-purple-800 hover:bg-purple-200"
           : "bg-purple-100 text-purple-800  border-purple-300 hover:bg-purple-200"
