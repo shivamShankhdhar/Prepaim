e081888eea7a52e17ea30eb1234bf0bf59aaff70
SHIVAM SHANKHDHAR <s.shankhdhar1981@gmail.com>
2024-07-21 11:36:32 +0530

    changes in subejct page height

--- server/controller/User/User.controller.js
+++ server/controller/User/User.controller.js
@@ -5,36 +5,35 @@ export const Login = async (req, res) => {
   try {
     const { userEmail } = req.body.data;
     const { userPassword } = req.body.data;
-    console.log(`login details`, userEmail, userPassword);
     // const isAdmin = true;
     const isExists = await User.findOne({
       email: userEmail,
       password: userPassword,
     });
     console.log(isExists);
-    // (isExists);
-    // if (isExists !== null) {
-    //   // if password, create jwt token
-    //   const token = jsonwebtoken.sign(
-    //     {
-    //       username: isExists.username,
-    //     },
-    //     "secret",
-    //     { expiresIn: "2h" }
-    //   );
-    //   return res.status(200).send({
-    //     msg: "user found",
-    //     token,
-    //     // isAdmin: isExists.isAdmin,
-    //     user_id: isExists._id || "",
-    //     user_profile_image: isExists.user_profile_image || "",
-    //     full_name:
-    //       `${isExists.first_name} ${isExists.last_name}` || "anonymous",
-    //     // user_email: isExists.email,
-    //   });
-    // } else {
-    //   return res.status(401).send({ error: "user not found" });
-    // }
+    isExists;
+    if (isExists !== null) {
+      // if password, create jwt token
+      const token = jsonwebtoken.sign(
+        {
+          userEmail: isExists.email,
+        },
+        "secret",
+        { expiresIn: "2h" }
+      );
+      return res.status(200).send({
+        message: "user found",
+        token,
+        // isAdmin: isExists.isAdmin,
+        user_id: isExists._id || "",
+        user_profile_image: isExists.user_profile_image || "",
+        full_name:
+          `${isExists.first_name} ${isExists.last_name}` || "anonymous",
+        // user_email: isExists.email,
+      });
+    } else {
+      return res.status(401).send({ error: "user not found" });
+    }
   } catch (error) {
     return res.status(501).send({ error: error.message });
   }
