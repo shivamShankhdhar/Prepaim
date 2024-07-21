876c25b64c0196bbd11ebce22fbb47e9bde23026
SHIVAM SHANKHDHAR <s.shankhdhar1981@gmail.com>
2024-07-21 14:27:12 +0530

    changes in subejct page height

--- client/middleware.ts
+++ client/middleware.ts
@@ -7,8 +7,8 @@ import type { NextRequest } from 'next/server'
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   
-// axios.defaults.headers.get["Content-Type"] =
-//     "application-json/x-www-form-urlencoded";
+axios.defaults.headers.get["Content-Type"] =
+    "application-json/x-www-form-urlencoded";
   
 //   console.log("middle ware called")
 
