74b643f1676a2232ab0f83e506d1a4f2d150251c
SHIVAM SHANKHDHAR <s.shankhdhar1981@gmail.com>
2024-07-21 16:41:33 +0530

    changes in subejct page height

--- server/routes/coding/coding.route.js
+++ server/routes/coding/coding.route.js
@@ -1,12 +1,22 @@
 import { Router } from "express";
 import * as Controller from "../../controller/coding/coding.controller.js";
 const codingRouter = Router();
+const corsOptions = {
+  origin: [
+    "https://prepaim.com",
+    "https://prepaim.in",
+    "http://localhost:3000",
+  ],
+  default: "https://prepaim.com",
+  // optionsSuccessStatus: 200,
+};
 
+app.use(cors(corsOptions));
 codingRouter
   .route("/getQuestionsFromCodingByQuestionName/:questionName")
-  .get(Controller.getQuestionsFromCodingByQuestionName);
+  .get(cors(corsOptions), Controller.getQuestionsFromCodingByQuestionName);
 
 codingRouter
   .route("/getAllLanguagesFromCoding")
-  .get(Controller.getAllLanguages);
+  .get(cors(corsOptions), Controller.getAllLanguages);
 export default codingRouter;
--- server/routes/mcq/route.js
+++ server/routes/mcq/route.js
@@ -2,22 +2,39 @@ import { Router } from "express";
 import * as Controller from "../../controller/mcq/api.controller.js";
 const mcqRouter = Router();
 
-mcqRouter.route("/postcomment").post(Controller.postComment);
-mcqRouter.route("/getallquestions").get(Controller.getAllQuestions);
-mcqRouter.route("/getallsubjects").get(Controller.getAllSubjects);
-mcqRouter.route("/getallbranches").get(Controller.getAllBranches);
+const corsOptions = {
+  origin: [
+    "https://prepaim.com",
+    "https://prepaim.in",
+    "http://localhost:3000",
+  ],
+  default: "https://prepaim.com",
+  // optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+mcqRouter.route("/postcomment").post(cors(corsOptions), Controller.postComment);
+mcqRouter
+  .route("/getallquestions")
+  .get(cors(corsOptions), Controller.getAllQuestions);
+mcqRouter
+  .route("/getallsubjects")
+  .get(cors(corsOptions), Controller.getAllSubjects);
+mcqRouter
+  .route("/getallbranches")
+  .get(cors(corsOptions), Controller.getAllBranches);
 // get routes
 mcqRouter
   .route("/getSubjectsbyBranchAndSubject/:branch")
-  .get(Controller.getSubjectsByBranch);
+  .get(cors(corsOptions),Controller.getSubjectsByBranch);
 mcqRouter
   .route("/getallchaptersbysubject/:subject")
-  .get(Controller.getAllChaptersBySubject);
+  .get(cors(corsOptions),Controller.getAllChaptersBySubject);
 mcqRouter
   .route("/getallquestionsbysubjectandchapter/:subject/:chapter")
-  .get(Controller.getQuestionsBySubjectAndChapter); // get all comments
+  .get(cors(corsOptions),Controller.getQuestionsBySubjectAndChapter); // get all comments
 mcqRouter
   .route("/getcommentbyquestion/:question")
-  .get(Controller.getCommentByQuestion); //get comment by question
+  .get(cors(corsOptions),Controller.getCommentByQuestion); //get comment by question
 
 export default mcqRouter;
