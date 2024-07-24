bd0c224a136e3eeae1061a16c70db81164fbdd6d
SHIVAM SHANKHDHAR <s.shankhdhar1981@gmail.com>
2024-07-24 06:03:29 +0530

    question mark added to questions of prepration mode

--- client/app/mcq/v1/components/Question/TestPreprationQuestionItem.tsx
+++ client/app/mcq/v1/components/Question/TestPreprationQuestionItem.tsx
@@ -68,7 +68,7 @@ const TestPreprationQuestionItem = ({
                     Q {questionNo}.
                     {questions.length > 0 &&
                       questions[questionNo - 1]?.question}
-                    ?
+                    &nbsp;?
                   </div>
                 </div>
                 {/* question guide  */}
