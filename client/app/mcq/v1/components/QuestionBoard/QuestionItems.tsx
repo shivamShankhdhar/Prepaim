0b1a03724edb61e6ab881bd4d80af24f0db349f7
SHIVAM SHANKHDHAR <s.shankhdhar1981@gmail.com>
2024-07-26 18:05:38 +0530

    tooltips updated

--- client/app/mcq/v1/components/QuestionBoard/QuestionItems.tsx
+++ client/app/mcq/v1/components/QuestionBoard/QuestionItems.tsx
@@ -22,51 +22,51 @@ const QuestionItems = ({ index, item }: any) => {
   const { question } = useParams();
   const questionNo = Number(question) - 1;
   return (
-    <Button
-      sx={{ minHeight: 0, minWidth: 0 }}
-      href={`/mcq/v1/${subject}/${chapter}/Test-Prepration-Mode/${index + 1}`}
-      className={`px-0 py-0 rounded-full h-9 w-9 ${
-        // on item level
-        item.level === "a"
-          ? `${
-              questionNo === index
-                ? "bg-indigo-600 focus:ring-2 focus:ring-inset focus:ring-white text-white hover:bg-indigo-700"
-                : "bg-indigo-100 focus:ring-2 focus:ring-inset focus:ring-white text-indigo-800 hover:bg-indigo-700 hover:text-white"
-            }`
-          : item.level === "b"
-          ? `${
-              questionNo === index
-                ? "bg-purple-600 text-white hover:bg-purple-900 focus:ring-2 focus:ring-inset focus:ring-white"
-                : "bg-purple-200 text-purple-900 focus:ring-2 focus:ring-white focus:ring-inset hover:bg-purple-600 hover:text-white"
-            }`
-          : `${
-              questionNo === index
-                ? "bg-rose-600 text-white hover:bg-rose-900 focus:ring-2 focus:ring-inset focus:ring-white"
-                : "bg-rose-200 text-rose-900 focus:ring-2 focus:ring-inset focus:ring-white hover:bg-rose-600 hover:text-white"
-            }`
-      } ${
-        // on active item
-        (questionNo === index &&
-          item.level === "a" &&
-          "bg-indigo-600  rounded-full focus:ring-2 focus:ring-inset focus:ring-white text-white hover:text-white hover:bg-indigo-700") ||
-        (questionNo === index &&
-          item.level === "b" &&
-          "  bg-purple-600  rounded-full focus:ring-2 focus:ring-inset focus:ring-white text-white hover:text-white hover:bg-purple-700") ||
-        (questionNo === index &&
-          item.level === "c" &&
-          "bg-rose-600 rounded-full text-white focus:ring-2 focus:ring-inset focus:ring-white hover:text-white hover:bg-rose-700")
+    <LightTooltip
+      title={`${
+        questionNo === index
+          ? "Current selected Question"
+          : "Click to navigate to this Question"
       }`}
     >
-      <LightTooltip
-        title={`${
-          questionNo === index
-            ? "Current selected Question"
-            : "Click to navigate to this Question"
+      <Button
+        sx={{ minHeight: 0, minWidth: 0 }}
+        href={`/mcq/v1/${subject}/${chapter}/Test-Prepration-Mode/${index + 1}`}
+        className={`px-0 py-0 rounded-full h-9 w-9 ${
+          // on item level
+          item.level === "a"
+            ? `${
+                questionNo === index
+                  ? "bg-indigo-600 focus:ring-2 focus:ring-inset focus:ring-white text-white hover:bg-indigo-700"
+                  : "bg-indigo-100 focus:ring-2 focus:ring-inset focus:ring-white text-indigo-800 hover:bg-indigo-700 hover:text-white"
+              }`
+            : item.level === "b"
+            ? `${
+                questionNo === index
+                  ? "bg-purple-600 text-white hover:bg-purple-900 focus:ring-2 focus:ring-inset focus:ring-white"
+                  : "bg-purple-200 text-purple-900 focus:ring-2 focus:ring-white focus:ring-inset hover:bg-purple-600 hover:text-white"
+              }`
+            : `${
+                questionNo === index
+                  ? "bg-rose-600 text-white hover:bg-rose-900 focus:ring-2 focus:ring-inset focus:ring-white"
+                  : "bg-rose-200 text-rose-900 focus:ring-2 focus:ring-inset focus:ring-white hover:bg-rose-600 hover:text-white"
+              }`
+        } ${
+          // on active item
+          (questionNo === index &&
+            item.level === "a" &&
+            "bg-indigo-600  rounded-full focus:ring-2 focus:ring-inset focus:ring-white text-white hover:text-white hover:bg-indigo-700") ||
+          (questionNo === index &&
+            item.level === "b" &&
+            "  bg-purple-600  rounded-full focus:ring-2 focus:ring-inset focus:ring-white text-white hover:text-white hover:bg-purple-700") ||
+          (questionNo === index &&
+            item.level === "c" &&
+            "bg-rose-600 rounded-full text-white focus:ring-2 focus:ring-inset focus:ring-white hover:text-white hover:bg-rose-700")
         }`}
       >
         {index + 1}
-      </LightTooltip>
-    </Button>
+      </Button>
+    </LightTooltip>
   );
 };
 
