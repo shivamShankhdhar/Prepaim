d5df005a78cab6a1e017143ca3a30ef0390941b1
SHIVAM SHANKHDHAR <s.shankhdhar1981@gmail.com>
2024-07-28 09:58:35 +0530

    changs in ui

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -30,15 +30,6 @@ export default function RootLayout({
 }) {
   return (
     <html>
-      <head>
-        <meta name="google-adsense-account" content="ca-pub-1113302487630583" />
-
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1113302487630583"
-        />
-        {/* <!-- prepaim_com --> */}
-      </head>
       <body className={Anek_Latin_fonts.className}>
         <CookiesProvider>
           <Header />
