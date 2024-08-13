09f13beb634098dd89ea0a0678bc6941816b300e
SHIVAM SHANKHDHAR <s.shankhdhar1981@gmail.com>
2024-08-13 20:18:37 +0530

    changes in ui

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -25,10 +25,19 @@ export default function RootLayout({
   return (
     <html>
       <head>
+        <meta
+          name="google-adsense-account"
+          content="ca-pub-1113302487630583"
+        ></meta>
         {/* Google tag (gtag.js) */}
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-QYJHFSJGRC"
+        ></script>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1113302487630583"
+          crossOrigin="anonymous"
         ></script>
       </head>
       <body className={poppins.className}>
