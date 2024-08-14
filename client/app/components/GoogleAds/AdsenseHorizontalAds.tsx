4c9476bf2b2ea6fb347e35906b124316035d470a
SHIVAM SHANKHDHAR <s.shankhdhar1981@gmail.com>
2024-08-14 08:21:25 +0530

    changes in ui

--- client/app/components/GoogleAds/AdsenseHorizontalAds.tsx
+++ client/app/components/GoogleAds/AdsenseHorizontalAds.tsx
@@ -1,21 +1,25 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const AdsenseHorizontalAds = () => {
+  const [isClient, setIsClient] = useState(false);
+  useEffect(() => setIsClient(true), []);
   useEffect(() => {
     ((window as any).adsbygoogle = (window as any).adsbygoogle || []).push({});
-  });
+  }, []);
   return (
-    <div className="w-full">
-      <ins
-        className="adsbygoogle"
-        style={{ display: "block" }}
-        data-ad-client="ca-pub-1113302487630583"
-        data-ad-slot="7957270938"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-      ></ins>
-    </div>
+    <>
+      {isClient && (
+        <ins
+          className="adsbygoogle"
+          style={{ display: "block" }}
+          data-ad-client="ca-pub-1113302487630583"
+          data-ad-slot="7957270938"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        />
+      )}
+    </>
   );
 };
 
