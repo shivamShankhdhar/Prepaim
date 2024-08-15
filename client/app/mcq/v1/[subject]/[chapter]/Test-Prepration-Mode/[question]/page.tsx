8540051529944b53e9801d23aeea1a5894a7daec
SHIVAM SHANKHDHAR <s.shankhdhar1981@gmail.com>
2024-08-15 16:41:41 +0530

    changes in ui

--- client/app/mcq/v1/[subject]/[chapter]/Test-Prepration-Mode/[question]/page.tsx
+++ client/app/mcq/v1/[subject]/[chapter]/Test-Prepration-Mode/[question]/page.tsx
@@ -217,7 +217,7 @@ const QuestionPage = () => {
               }}
               data-ad-client="ca-pub-1113302487630583"
               data-ad-slot="7957270938"
-              data-ad-format=" horizontal"
+              data-ad-format="horizontal"
               data-full-width-responsive="true"
             ></ins>
           </div>
