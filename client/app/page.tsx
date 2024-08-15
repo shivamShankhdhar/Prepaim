9635edacec26b59b9fbed6edac7fb5c53c1e720e
SHIVAM SHANKHDHAR <s.shankhdhar1981@gmail.com>
2024-08-15 15:42:36 +0530

    changes in ui

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -72,6 +72,13 @@ export default function Home() {
             )}
           </div>
         </div>
+        <ins
+          className="adsbygoogle text-center"
+          style={{ display: "block" }}
+          data-ad-format="autorelaxed"
+          data-ad-client="ca-pub-1113302487630583"
+          data-ad-slot="7384794981"
+        ></ins>
         <Footer />
       </div>
     </>
