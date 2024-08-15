7319efea13f08ad79d723df90515234512e87a46
SHIVAM SHANKHDHAR <s.shankhdhar1981@gmail.com>
2024-08-15 15:51:52 +0530

    changes in ui

--- client/app/mcq/v1/[subject]/[chapter]/page.tsx
+++ client/app/mcq/v1/[subject]/[chapter]/page.tsx
@@ -39,61 +39,98 @@ const ChapterPage = () => {
      ((window as any).adsbygoogle = (window as any).adsbygoogle || []).push({});
    }, []);
 
-   return (
-     <div className="flex flex-col items-center  w-full">
-       {/* <script
+  return (
+    <div className="w-full flex justify-between">
+      {/* left side of the screen */}
+      <div className="w-[100px] h-[92vh]">
+        <div className="w-full inline-block justify-center items-center text-center">
+          <ins
+            className="adsbygoogle text-center"
+            style={{
+              display: "inline-block",
+              width: "100%",
+              height: "100px",
+            }}
+            data-ad-client="ca-pub-1113302487630583"
+            data-ad-slot="7957270938"
+            data-ad-format="veritical"
+            data-full-width-responsive="true"
+          ></ins>
+        </div>
+      </div>
+      {/* mid side of the screen */}
+      <div className="flex flex-col items-center w-full">
+        {/* <script
          async
          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1113302487630583"
          crossOrigin="anonymous"
        ></script> */}
-       <Breadcrum chaptersLength={chapters.length} />
-       <div className="w-full inline-block justify-center items-center text-center">
-         <ins
-           className="adsbygoogle text-center"
-           style={{
-             display: "inline-block",
-             width: "100%",
-             height: "100px",
-           }}
-           data-ad-client="ca-pub-1113302487630583"
-           data-ad-slot="7957270938"
-           data-ad-format=" horizontal"
-           data-full-width-responsive="true"
-         ></ins>
-       </div>
-       <div className="w-full px-2 min-h-[92vh] max-[fit-content]">
-         <div className="w-full gap-1/2 bg-white px-5 py-0 flex flex-col mt-1 rounded-md border">
-           {isLoading === true ? (
-             <ChapterItemSkeleton />
-           ) : error === "" ? (
-             chapters.length > 0 ? (
-               chapters.map((data, index) => {
-                 return (
-                   <ChapterItem
-                     key={index}
-                     chapterItemLength={chapters.length}
-                     data={data}
-                     index={index}
-                     subject={subject}
-                   />
-                 );
-               })
-             ) : (
-               <ErrorMessage
-                 text={"No chapters Found"}
-                 isBg={false}
-                 isButton={false}
-               />
-             )
-           ) : (
-             <ErrorMessage text={error} isButton={true} isBg={false} />
-           )}
-         </div>
-       </div>
+        <Breadcrum chaptersLength={chapters.length} />
+        <div className="w-full inline-block justify-center items-center text-center">
+          <ins
+            className="adsbygoogle text-center"
+            style={{
+              display: "inline-block",
+              width: "100%",
+              height: "100px",
+            }}
+            data-ad-client="ca-pub-1113302487630583"
+            data-ad-slot="7957270938"
+            data-ad-format="horizontal"
+            data-full-width-responsive="true"
+          ></ins>
+        </div>
+        <div className="w-full px-2 min-h-[92vh] max-[fit-content]">
+          <div className="w-full gap-1/2 bg-white px-5 py-0 flex flex-col mt-1 rounded-md border">
+            {isLoading === true ? (
+              <ChapterItemSkeleton />
+            ) : error === "" ? (
+              chapters.length > 0 ? (
+                chapters.map((data, index) => {
+                  return (
+                    <ChapterItem
+                      key={index}
+                      chapterItemLength={chapters.length}
+                      data={data}
+                      index={index}
+                      subject={subject}
+                    />
+                  );
+                })
+              ) : (
+                <ErrorMessage
+                  text={"No chapters Found"}
+                  isBg={false}
+                  isButton={false}
+                />
+              )
+            ) : (
+              <ErrorMessage text={error} isButton={true} isBg={false} />
+            )}
+          </div>
+        </div>
+        {/* right side of the screen */}
+        <div className="w-[100px] h-[92vh]">
+          <div className="w-full inline-block justify-center items-center text-center">
+            <ins
+              className="adsbygoogle text-center"
+              style={{
+                display: "inline-block",
+                width: "100%",
+                height: "100px",
+              }}
+              data-ad-client="ca-pub-1113302487630583"
+              data-ad-slot="7957270938"
+              data-ad-format="veritical"
+              data-full-width-responsive="true"
+            ></ins>
+          </div>
+        </div>
+      </div>
 
-       <Footer />
-     </div>
-   );
+      <Footer />
+    </div>
+  );
 };
 
 export default ChapterPage;
