9cf462b368bf4389275561b94e909b3a5dee65d1
SHIVAM SHANKHDHAR <s.shankhdhar1981@gmail.com>
2024-08-19 16:51:09 +0530

    changes in ui

--- client/app/mcq/v1/[subject]/[chapter]/Test-Prepration-Mode/[question]/layout.tsx
+++ client/app/mcq/v1/[subject]/[chapter]/Test-Prepration-Mode/[question]/layout.tsx
@@ -1,9 +1,3 @@
-export const metadata = {
-  title: "Question | Prepaim.com",
-  description:
-    "Prepaim.com helps students to prepare for the examination related to the enginnering for higher education",
-};
-
 export default function QuestionLayout({
   children,
 }: {
