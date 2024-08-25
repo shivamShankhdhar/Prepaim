d3799fa3e91841a65bbf52c3dbd8bd87a3b405d6
SHIVAM SHANKHDHAR <s.shankhdhar1981@gmail.com>
2024-08-25 20:53:34 +0530

    changes in ui

--- client/app/blog/[slug]/page.tsx
+++ client/app/blog/[slug]/page.tsx
@@ -28,7 +28,7 @@ const BlogPost = () => {
       .get(
         `https://api.data.admin-panel.prepaim.com/blog/getBlogPostByTitle/${slug
           .toString()
-          .replaceAll("-", " ")}`
+          .replaceAll("%20", " ")}`
       )
       .then((res) => {
         setPost(res.data);
