82d0d21b3dfff1a556358732501296b3bca7a119
SHIVAM SHANKHDHAR <s.shankhdhar1981@gmail.com>
2024-08-25 17:56:28 +0530

    changes in ui

--- client/app/components/Global/FormatedDate.tsx
+++ client/app/components/Global/FormatedDate.tsx
@@ -4,10 +4,7 @@ import { format } from "date-fns";
 const FormatedDate = ({ date, cls, isTime }: any) => {
   return (
     <div className={`${cls || "text-[11px]"}`}>
-      {isTime === false
-        ? format(new Date(date), "eee, MMM dd, yyyy ")
-        : format(new Date(date), "eee, MMM dd, yyyy / hh:mm a")}
-      {}
+      {format(new Date(date), "eee, MMM dd, yyyy / hh:mm a")}
     </div>
   );
 };
