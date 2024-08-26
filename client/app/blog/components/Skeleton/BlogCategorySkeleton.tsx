720aef41241338526e4ebf5aba615af0342fb153
SHIVAM SHANKHDHAR <s.shankhdhar1981@gmail.com>
2024-08-26 09:34:29 +0530

    changes in ui

--- client/app/blog/components/Skeleton/BlogCategorySkeleton.tsx
+++ client/app/blog/components/Skeleton/BlogCategorySkeleton.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const BlogCategorySkeleton = () => {
   const items = [1, 2, 3, 4, 5];
   return (
-    <div className="w-full flex gap-2">
+    <div className="w-full flex gap-2 flex-wrap">
       {items.map((item) => (
         <div
           key={`key-for-blog-category-skeleton-${item}`}
