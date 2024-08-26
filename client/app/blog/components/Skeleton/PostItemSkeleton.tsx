6870ea3fd324aafa8dcf33f677f84b8e1869d544
SHIVAM SHANKHDHAR <s.shankhdhar1981@gmail.com>
2024-08-26 09:55:34 +0530

    changes in ui

--- client/app/blog/components/Skeleton/PostItemSkeleton.tsx
+++ client/app/blog/components/Skeleton/PostItemSkeleton.tsx
@@ -3,14 +3,14 @@ import { CiImageOn } from "react-icons/ci";
 const PostItemSkeleton = () => {
   const items = [1, 2, 3, 4, 5];
   return (
-    <div className="w-full flex gap-2">
+    <div className="w-full flex gap-2 flex-wrap">
       {items.map((item) => (
         <div
           key={`skeleton-post-item-${item}`}
           className="flex gap-2 sm:flex-col max-sm:flex-col md:flex-col max-md:flex-col lg:flex-row xl:flex-row 2xl:flex-row 3xl:flex-row rounded-md animate-pulse bg-purple-100"
         >
           <div className="sm:w-full max-sm:w-full md:w-full max-md:w-full lg:w-[300px] xl:w-[300px] h-[300px] animate-pulse">
-            <CiImageOn size={400} />
+            <CiImageOn size={300} />
           </div>
 
           <div className="flex-1 h-[300px] bg-purple-200 animate-pulse"></div>
