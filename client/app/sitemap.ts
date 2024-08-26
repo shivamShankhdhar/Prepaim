0b3e80a6911ed11e12b2e8fbc61fda2a9a7d0497
SHIVAM SHANKHDHAR <s.shankhdhar1981@gmail.com>
2024-08-26 08:16:14 +0530

    changes in ui

--- client/app/sitemap.ts
+++ client/app/sitemap.ts
@@ -113,7 +113,16 @@ export default async function sitemap() {
       url: `${baseURL}/blog`,
       lastModified: new Date().toISOString(),
     },
+   
     {
+      url: `${baseURL}/mcq/v1/subjects`,
+      lastModified: new Date().toISOString(),
+    },
+    ...allChaptersForSpecificSubject,
+    ...allPostsURL,
+    ...urlSetTestPreprationMode,
+    ...urlSetPreprationMode,
+     {
       url: `${baseURL}/about`,
       lastModified: new Date().toISOString(),
     },
@@ -129,13 +138,5 @@ export default async function sitemap() {
       url: `${baseURL}/terms-of-services`,
       lastModified: new Date().toISOString(),
     },
-    {
-      url: `${baseURL}/mcq/v1/subjects`,
-      lastModified: new Date().toISOString(),
-    },
-    ...allChaptersForSpecificSubject,
-    ...allPostsURL,
-    ...urlSetTestPreprationMode,
-    ...urlSetPreprationMode
   ];
 }
