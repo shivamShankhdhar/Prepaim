bb6c0e1bdd9385ea954f592e2a0a1def2f4f89e7
SHIVAM SHANKHDHAR <s.shankhdhar1981@gmail.com>
2024-08-26 14:11:26 +0530

    changes in ui

--- client/app/blog/components/BlogShareButton.tsx
+++ client/app/blog/components/BlogShareButton.tsx
@@ -44,10 +44,10 @@ const BlogShareButton = ({ cls }: Props) => {
     <LightTooltip title={`Share this question`}>
       <Button
         // sx={{ border: 1 }}
-        className={`w-[fit-content] rounded-full flex gap-2 py-3 px-2 justify-center cursor-pointer items-center border  ${
+        className={`w-[fit-content] rounded-full flex gap-2 py-2 px-1 justify-center cursor-pointer items-center aspect-square ${
           linkCopied
             ? " bg-green-200 text-green-950  hover:bg-green-300"
-            : " bg-gray-200  hover:bg-gray-300 text-purple-800"
+            : " bg-white  hover:bg-purple-100 text-purple-800"
         }`}
         onClick={() => handleShare()}
       >
