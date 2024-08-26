037343074f8366ae593d9d5d5e3e7869117916dc
SHIVAM SHANKHDHAR <s.shankhdhar1981@gmail.com>
2024-08-26 09:59:26 +0530

    changes in ui

--- client/app/blog/components/Skeleton/PostItemSkeleton.tsx
+++ client/app/blog/components/Skeleton/PostItemSkeleton.tsx
@@ -7,13 +7,19 @@ const PostItemSkeleton = () => {
       {items.map((item) => (
         <div
           key={`skeleton-post-item-${item}`}
-          className="flex gap-2 sm:flex-col max-sm:flex-col md:flex-col max-md:flex-col lg:flex-row xl:flex-row 2xl:flex-row 3xl:flex-row rounded-md animate-pulse bg-purple-100"
+          className="flex gap-2 sm:flex-col max-sm:flex-col md:flex-col max-md:flex-col lg:flex-row xl:flex-row 2xl:flex-row 3xl:flex-row rounded-md animate-pulse "
         >
           <div className="sm:w-full max-sm:w-full md:w-full max-md:w-full lg:w-[300px] xl:w-[300px] h-[300px] animate-pulse">
             <CiImageOn size={300} />
           </div>
 
-          <div className="flex-1 h-[300px] bg-purple-200 animate-pulse"></div>
+          <div className="flex-1 h-[300px] flex flex-col gap-1">
+            <div className="flex w-full bg-purple-200 animate-pulse py-6"></div>
+            <div className="flex w-full bg-purple-200 animate-pulse py-10"></div>
+            <div className="flex w-full bg-purple-200 animate-pulse py-6 ">
+              <div className="w-[150px] py-4 bg-purple-200 animate-pulse"></div>
+            </div>
+          </div>
         </div>
       ))}
     </div>
